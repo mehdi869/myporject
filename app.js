20191d557c47d8e14da9ecf7be5482b9d7765adc
mehdi <armehdi0101@gmail.com>
2024-11-18 18:57:58 +0100

    creating hello message

---
+++ app.js
@@ -0,0 +1,7 @@
+let fullname = "mehdi";
+let age = 19;
+let student = true;
+
+document.getElementById("p1").textContent = "your name is " + fullname;
+document.getElementById("p2").textContent = "you are" + age + "years old";
+document.getElementById("p3").textContent = "Enrolled:" + student;
